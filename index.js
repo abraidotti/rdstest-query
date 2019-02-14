d6fb044150d207edd6e170cc81b394894bae5211
abraidotti <alessandro.braidotti@gmail.com>
2019-02-14 09:28:56 -0500

    fix syntax

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sql = require('mssql')
 
-exports handler = async function tryQuery(event) {
+async function tryQuery() {
     try {
         await sql.connect('mssql://rdstest:rdstestpwd@rdstest.cnx7jaqol3o2.us-east-1.rds.amazonaws.com:1433')
         const result = await sql.query `select @@version`
@@ -8,4 +8,6 @@ exports handler = async function tryQuery(event) {
     } catch (err) {
         console.log(err)
     }
-}
+};
+
+tryQuery();
